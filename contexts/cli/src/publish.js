d2283a599be10b8df28bdf92e19778b68d942089
alejooroncoy <adoa2705@gmail.com>
2023-01-16 18:34:10 -0500

    refactor(cli): Implement mediafire and build to file .exe 📦👨‍💻

--- contexts/cli/src/publish.js
+++ contexts/cli/src/publish.js
@@ -1,5 +1,7 @@
 import fetch from "node-fetch";
+import prompts from "prompts";
 import * as esbuild from "esbuild";
+import exe from "@angablue/exe";
 import path from "node:path";
 import packageJson from "../package.json" assert { type: "json" };
 import { config as configDotenv } from "dotenv";
@@ -9,14 +11,14 @@ configDotenv();
 const { version } = packageJson;
 
 const pathPsmJs = path.resolve("./dist/psm.js");
+const output = path.resolve("./dist/psm.exe");
 
 const baseUrl = process.env.BASE_URL;
 
-const builded = await esbuild.build({
+await esbuild.build({
   entryPoints: ["./src/cli.js"],
   outfile: pathPsmJs,
   bundle: true,
-  write: false,
   target: "node18",
   platform: "node",
   define: {
@@ -24,17 +26,42 @@ const builded = await esbuild.build({
   },
 });
 
-const { text: contentPsmJs } = builded.outputFiles.at(0);
-
-const headersFetch = new Headers();
-headersFetch.append("Content-Type", "application/javascript;charset=UTF-8");
-
-const response = await fetch(`${baseUrl}/psm/bin?version=${version}`, {
-  method: "POST",
-  body: contentPsmJs,
-  headers: headersFetch,
+await exe({
+  entry: pathPsmJs,
+  out: output,
+  pkg: ["-C", "GZip"], // Specify extra pkg arguments
+  version,
+  target: "latest-win-x64",
+  icon: "./src/assets/logo-psm.ico", // Application icons must be in .ico format
+  properties: {
+    CompanyName: "PSM-Team",
+    ProductName: "PSM Bin",
+    FileDescription: "Pes6 server manager version Bin. 💻⚽",
+    LegalCopyright: "Copyright © PSM-Team MIT License",
+  },
 });
 
-const data = await response.json();
+const urlMediafire = await prompts({
+  type: "text",
+  name: "value",
+  message: "Write url mediafire of the file",
+});
 
-console.log(data);
+const headersFetch = new Headers();
+
+headersFetch.append("Content-Type", "application/json; charset=utf-8");
+
+if (urlMediafire.value || process.env.URL_MEDIAFIRE) {
+  const response = await fetch(`${baseUrl}/psm/bin?version=${version}`, {
+    method: "POST",
+    body: JSON.stringify({
+      url: process.env.URL_MEDIAFIRE || urlMediafire.value,
+    }),
+    headers: headersFetch,
+  });
+  const data = await response.json();
+  console.log(data);
+  process.exit(0);
+}
+
+console.log("Missing url mediafire");
