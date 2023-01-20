a4c3bf19443d05d263ba7ce75f05bf0e1c0f4a2e
alejooroncoy <adoa2705@gmail.com>
2023-01-20 13:29:37 -0500

    refactor(frontend): Update project | Add hero and servers 👨‍💻✨

--- contexts/frontend/astro.config.mjs
+++ contexts/frontend/astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), tailwind()]
 });
--- contexts/frontend/tailwind.config.cjs
+++ contexts/frontend/tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#247291",
+        secondary: "#F8DA5B",
+        thirdary: "#EEF2E2",
+        quaternary: "#F5F9EE",
+      },
+      flex: {
+        full: "100%",
+      },
+    },
+  },
+  plugins: [],
+};
