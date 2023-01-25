c8f4ca36e788d2c47000eca45c29929076915231
alejooroncoy <adoa2705@gmail.com>
2023-01-25 16:04:01 -0500

    feat(backend): Add cors 👨‍💻📦✨

--- contexts/backend/src/index.js
+++ contexts/backend/src/index.js
@@ -1,12 +1,17 @@
 import express from "express";
+import cors from "cors";
 import config from "./config/index.js";
 import server from "./apps/server/routes.js";
 import psm from "./apps/psm/routes.js";
 import noFoundHandler from "./middlewares/notFound.handler.js";
+import proposal from "./apps/proposal/routes.js";
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+app.use("/proposal", proposal);
 app.use("/server", server);
 app.use("/psm", psm);
 
