38034bd7ccdd04604838fd3c52f8919b269d1e7a
alejooroncoy <adoa2705@gmail.com>
2023-01-25 16:32:21 -0500

    feat(frontend): Add tailwindcss 🦄👨‍💻

--- contexts/frontend/astro.config.mjs
+++ contexts/frontend/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -8,7 +8,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), tailwind()]
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    tailwind(),
+  ],
 });
