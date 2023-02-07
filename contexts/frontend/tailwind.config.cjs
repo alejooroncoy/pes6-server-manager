ae1ef2e88bda134fd53af006b824965ef9079ebf
alejooroncoy <adoa2705@gmail.com>
2023-02-07 16:50:19 -0500

    feat(frontend): Add animation 👨‍💻✨

--- contexts/frontend/tailwind.config.cjs
+++ contexts/frontend/tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      keyframes: {
+        pulseButton: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(0.95)" },
+        },
+      },
       colors: {
         primary: "#247291",
         secondary: "#F8DA5B",
