a912503e85fb2656f6ec975563ab891e7c69fa37
alejooroncoy <adoa2705@gmail.com>
2023-02-07 15:10:10 -0500

    refactor(app): From app -> pages 👨‍💻📦✨

---
+++ contexts/app/src/pages/index.jsx
@@ -0,0 +1,46 @@
+import dynamic from "next/dynamic";
+import HomeHero from "../components/Home/HomeHero";
+import HomeMessage from "../components/Home/HomeMessage";
+import HomeServers from "../components/Home/HomeServers";
+import getServers from "../utils/getServers";
+
+const HomeOpenPes6 = dynamic(() => import("../components/Home/HomeOpenPes6"), {
+  ssr: false,
+});
+
+const HomeSerial = dynamic(() => import("../components/Home/HomeSerial"), {
+  ssr: false,
+});
+
+const Home = ({ fontFamily, servers }) => {
+  return (
+    <main
+      className={["min-h-screen flex flex-wrap", fontFamily.className].join(
+        " "
+      )}
+    >
+      <HomeHero />
+      <section className="flex-[70%] md:pl-6">
+        <HomeServers servers={servers} />
+        <HomeOpenPes6 />
+      </section>
+      <aside className="flex-1 lg:pr-6 lg:px-0 px-6">
+        <div className="py-4 flex flex-col gap-4 h-full justify-center items-center">
+          <HomeSerial />
+          <HomeMessage />
+        </div>
+      </aside>
+    </main>
+  );
+};
+
+export default Home;
+
+export const getStaticProps = async () => {
+  const servers = await getServers();
+  return {
+    props: {
+      servers,
+    },
+  };
+};
