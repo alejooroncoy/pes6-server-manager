2ef6c964d70b6a88a0db88d42451f28ae3c55685
alejooroncoy <adoa2705@gmail.com>
2023-02-07 15:05:46 -0500

    feat(app): Add usePathPes6, useSerial and change useServers 👨‍💻⚓🌟

--- contexts/app/src/hooks/useServers.js
+++ contexts/app/src/hooks/useServers.js
@@ -1,32 +1,53 @@
 import { useEffect, useState } from "react";
-import {
-  configHostCache,
-  existsCache,
-  initialCache,
-  setCache,
-} from "../config/cache";
+import { configHostCache, setCache, existsCache } from "../config/cache";
 import loadHost from "../utils/loadHost";
+import hosts from "../libs/hosts";
 
 export default function useServers(serversGetted) {
-  const [servers] = useState(serversGetted);
+  const [servers, setServers] = useState(serversGetted);
+  const [serverPlaying, setServerPlaying] = useState("");
+
   const chooseServer = async (serverName) => {
     if (!configHostCache.length) await setCache(servers, "hosts");
     await loadHost(serverName);
+    setServerPlaying(serverName);
   };
 
   const loadServers = async () => {
     const existsDirCache = await existsCache();
     if (!existsDirCache) await initialCache();
     await setCache(servers, "hosts");
+    const hostDefault = await hosts.getHostDefault();
+    setServerPlaying(hostDefault);
   };
 
   useEffect(() => {
     loadServers();
   }, []);
+
+  useEffect(() => {
+    setServers(
+      servers.map((server) => {
+        if (server.activate)
+          return {
+            ...server,
+            activate: false,
+          };
+        if (server.name === serverPlaying)
+          return {
+            ...server,
+            activate: true,
+          };
+        return server;
+      })
+    );
+  }, [serverPlaying]);
   return [
     servers,
     {
       chooseServer,
+      serverPlaying,
+      setServers,
     },
   ];
 }
