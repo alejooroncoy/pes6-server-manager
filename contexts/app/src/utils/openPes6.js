c1a814955d0ba94f198111ea6be31b2b8d87ab14
alejooroncoy <adoa2705@gmail.com>
2023-02-07 15:09:11 -0500

    fix(app): Change serialPs2, and add attemps in getServers 👨‍💻✨

---
+++ contexts/app/src/utils/openPes6.js
@@ -0,0 +1,19 @@
+import logger from "../libs/logger";
+import { Command } from "@tauri-apps/api/shell";
+import locationPes6 from "../libs/locationPes6";
+import { getIsWsl, isWindows } from "./getOs";
+
+export default async function openPes6() {
+  const locationPes = await locationPes6.getLocationPes();
+  const commandStartPes6 = await locationPes6.getCommandStartPes6();
+  logger.log(`Open pes6 from ${locationPes} ✨⚽`);
+  const osIsWindows = await isWindows();
+  const isWsl = await getIsWsl();
+  if (osIsWindows || isWsl) {
+    const command = new Command("powershell", commandStartPes6, {
+      encoding: "utf-8",
+      cwd: locationPes.slice(0, -1),
+    });
+    await command.spawn();
+  }
+}
