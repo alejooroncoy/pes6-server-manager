f2afbaa3462e6899e8a48943accf0d8152c3a0a7
alejooroncoy <adoa2705@gmail.com>
2023-02-12 09:09:22 -0500

    feat(backend): Add github 👨‍💻✨

---
+++ contexts/backend/src/libs/github.js
@@ -0,0 +1,21 @@
+import config from "../config";
+
+const github = {
+  async getReleaseByTag(tag) {
+    const response = await fetch(`${config.githubReleaseBase}/tags/${tag}`);
+    const release = await response.json();
+    return release;
+  },
+  async getReleaseLatest() {
+    const response = await fetch(`${config.githubReleaseBase}/latest`);
+    const release = await response.json();
+    return release;
+  },
+  async getSignature(browserDownloadUrl) {
+    const response = await fetch(browserDownloadUrl);
+    const signature = await response.text();
+    return signature;
+  },
+};
+
+export default github;
