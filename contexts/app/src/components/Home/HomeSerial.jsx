4c77c7bba7a1d62a56fd0769cfcda181576a0b26
Alejo Oroncoy <adoa2705@gmail.com>
2023-02-19 12:43:57 -0500

    fix(app): Fix bugs in links, open pes6, and update layout 👨‍💻✨

--- contexts/app/src/components/Home/HomeSerial.jsx
+++ contexts/app/src/components/Home/HomeSerial.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useRef } from "react";
-import { Dropdown } from "@nextui-org/react";
 import useSerial from "../../hooks/useSerial";
 import logger from "../../libs/logger";
 
 const HomeSerial = () => {
   const inputRef = useRef();
-  const [serial, setSerial, restoreSerial, serials] = useSerial();
+  const [serial, setSerial, restoreSerial] = useSerial();
 
   const handleSubmitChangeSerial = async (e) => {
     e.preventDefault();
@@ -46,25 +45,6 @@ const HomeSerial = () => {
             defaultValue="Looking for your serial 📃"
             maxLength={20}
           />
-          <Dropdown type="menu">
-            <Dropdown.Button
-              className="flex-full"
-              fit="true"
-              css={{
-                bgColor: "#247291",
-              }}
-            >
-              Serials used
-            </Dropdown.Button>
-            <Dropdown.Menu
-              onAction={handleSelectSerial}
-              aria-label="Serials used"
-            >
-              {serials.map((serialGetted) => (
-                <Dropdown.Item key={serialGetted}>{serialGetted}</Dropdown.Item>
-              ))}
-            </Dropdown.Menu>
-          </Dropdown>
           <button className="py-2 active:scale-95 transition-transform duration-200 font-bold rounded-md px-2 bg-primary text-white flex-full sm:flex-1">
             Change the serial!
           </button>
