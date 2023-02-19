83faa338fbbc97c27e7b1eb23b61c80d8a4ac2fa
Alejo Oroncoy <adoa2705@gmail.com>
2023-02-19 12:48:42 -0500

    refactor(app): From spawn to execute 👨‍💻✨

--- contexts/app/src/utils/openPes6.js
+++ contexts/app/src/utils/openPes6.js
@@ -6,14 +6,24 @@ import { getIsWsl, isWindows } from "./getOs";
 export default async function openPes6() {
   const locationPes = await locationPes6.getLocationPes();
   const commandStartPes6 = await locationPes6.getCommandStartPes6();
-  logger.log(`Open pes6 from ${locationPes} ✨⚽`);
   const osIsWindows = await isWindows();
   const isWsl = await getIsWsl();
   if (osIsWindows || isWsl) {
-    const command = new Command("powershell", commandStartPes6, {
-      encoding: "utf-8",
-      cwd: locationPes,
-    });
-    await command.spawn();
+    try {
+      const command = new Command("powershell", commandStartPes6, {
+        encoding: "utf-8",
+        cwd: locationPes,
+      });
+      const executed = await command.execute();
+      if (executed.stderr.length) {
+        logger.error(
+          "We couldn't open pes6 😢. We recommend changing the location of the folder where your PES6.exe is located 📁"
+        );
+        return;
+      }
+      logger.log(`Open pes6 from ${locationPes} ✨⚽`);
+    } catch (err) {
+      logger.error("We couldn't open pes6 😢");
+    }
   }
 }
